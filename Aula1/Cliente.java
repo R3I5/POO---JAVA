f9f718fdd26395cbd76fb4833816c4ae1b729232
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-04-08 20:58:20 -0300

    Aula 1 POO

---
+++ Aula1/Cliente.java
@@ -0,0 +1,29 @@
+package Aula1;
+
+import java.sql.Date;
+
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private Date dataDeAniversario;
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getCpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public Date getDatadeAniversario(){
+        return dataDeAniversario;
+    }
+    public void setDataDeAniversario(Date dataDeAniversario){
+        this.dataDeAniversario = dataDeAniversario;
+    }
+}
